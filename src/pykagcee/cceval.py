d3153d413e0a4fd789979e883ba3630755581231
Oscar Arambula <peteroso876@gmail.com>
2025-03-05 00:11:49 -0600

    feat: first steps to reproduce CCEval

---
+++ src/pykagcee/cceval.py
@@ -0,0 +1,22 @@
+import json
+from typing import Generator, TypedDict
+
+
+class TaskMetadata(TypedDict):
+    task_id: str
+    repository: str
+    file: str
+    context_start_linen: int
+    groundtruth_start_lineno: int
+    right_context_start_lineno: int
+
+class Task(TypedDict):
+    prompt: str
+    groundtruth: str
+    right_context: str
+    metadata: TaskMetadata
+
+def iter_tasks(file_path: str) -> Generator[Task, None, None]:
+    with open(file_path, "r", encoding='utf-8') as file:
+        for line in file:
+            yield json.loads(line)
--- src/pykagcee/main.py
+++ src/pykagcee/main.py
@@ -1,10 +1,14 @@
+import time
+from concurrent.futures import ThreadPoolExecutor
 from graph_database import GraphDatabaseHandler
 from graph_database.build import build_graph_database
 import typer
 import sys
 import os
 from graph_database import indexer
+from pykagcee import cceval
 from .config import NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD
+from pykagcee.checkpoint import Checkpoint
 
 app = typer.Typer()
 
@@ -27,20 +31,61 @@ graph_db = GraphDatabaseHandler(
     use_lock=True,
 )
 
+checkpoint = Checkpoint(graph_db.graph)
 
 @app.command()
-def build(project_path: str) -> None:
+def mark_as_completed(task_id: str) -> None:
+    """
+    Mark a task as completed.
+    """
+    checkpoint.mark_as_completed(task_id)
+
+@app.command()
+def build(project_path: str, task_id: str) -> None:
     """
     Build the graph database for the given project path.
     """
+    if task_id in checkpoint:
+        print(f"Task {task_id} already completed.")
+        return
+
     build_graph_database(
         graph_db=graph_db,
         repo_path=project_path,
-        task_id="pykagcee",
+        task_id=task_id,
         is_clear=True,
         max_workers=8,
         env_path_dict=env_path_dict,
     )
+
+    checkpoint.mark_as_completed(task_id)
+
+@app.command()
+def build_fake(project_path: str, task_id: str) -> None:
+    """
+    Build the graph database for the given project path.
+    """
+    if task_id in checkpoint:
+        print(f"Task {task_id} already completed.")
+        return
+
+    time.sleep(1)
+
+    checkpoint.mark_as_completed(task_id)
+
+@app.command()
+def init_cceval(tasks_file_path: str, raw_data_directory: str) -> None:
+    """
+    Initialize the CCEval data needed to build the graph.
+    """
+    repositories = { task["metadata"]["repository"] for task in cceval.iter_tasks(tasks_file_path) }
+
+    projects = [ os.path.join(raw_data_directory, repository) for repository in repositories ]
+
+    with ThreadPoolExecutor(max_workers=16) as executor:
+        executor.map(build_fake, projects, repositories)
+
+    print("Done.")
 
 @app.command()
 def wipe() -> None:
@@ -53,3 +98,7 @@ def wipe() -> None:
         graph_db.execute_query(f"DROP INDEX {index_name}")
 
     print("Database wiped.")
+
+@app.command()
+def play() -> None:
+    print(os.path.join("src", "pykagcee", "main.py"))
